25767eac7f063457424959b332f9c12c7ac4f4eb
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2025-04-24 18:14:57 +0500

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,133 +1,141 @@
 const lenis = new Lenis({
   autoRaf: true,
 });
-// Split text animation
-const text = SplitType.create(".title", { types: "words" });
 
-//fade up
-const fadeUp = {
-  autoAlpha: 0,
-  y: 50,
-  ease: "power2.out",
-};
-//fade right
-const fadeRight = {
-  autoAlpha: 0,
-  x: 50,
-  ease: "power2.out",
-};
-//fade left
-const fadeLeft={
-  autoAlpha: 0,
-  ease:"power2.out"
+function raf(time) {
+  lenis.raf(time);
+  ScrollTrigger.update();
+  requestAnimationFrame(raf);
 }
-//fade left delayed
-const headerElements = {
-  ...fadeLeft,
-  x:-10,
-  stagger: 0.1,
-  delay: 0.4,
-};
-
-//fade left elements
-gsap.from(".icon-link", headerElements);
-gsap.from(".nav-item", headerElements);
-
-//fade up elements
-gsap.from("h1 .word", {
-  ...fadeUp,
-  duration: 0.7,
-  stagger: 0.07,
-  ease: "back.out(1.7)",
-  delay: 0.4,
-});
-
-gsap.from(".hero-image",{
-  scale:0.8,
-  ...fadeLeft,
-  delay:0.5
-})
-
-gsap.from(".sub-title, .hero-button,.contact-card", {
-  ...fadeUp,
-  duration: 0.95,
-  delay: 0.6,
-  stagger: 0.1,
-});
-
-
-gsap.utils.toArray(".card:not(.service-card)").forEach((card) => {
-  gsap.from(card, {
-    scale: 0.7,
+requestAnimationFrame(raf);
+document.addEventListener("DOMContentLoaded", () => {
+  // Split text animation
+  const text = SplitType.create(".title", { types: "words" });
+  
+  //fade up
+  const fadeUp = {
     autoAlpha: 0,
-    ease: "power4.out",
-    scrollTrigger: {
-      trigger: card,
-      scrub: true,
-      start: "top bottom",
-      end: "center bottom",       
-    }
-  });
-});
-
-gsap.utils.toArray(".accordion-item").forEach((item)=>{
-  gsap.from(item,{
-    scale:0.5,
-    x:40,
-    opacity:0,
-    ease: "power4.out",
-    scrollTrigger: {
-      trigger: item,
-      scrub: true,
-      start: "top bottom",
-      end: "bottom 70%",       
-    }
-  })
-})
-
-gsap.utils.toArray(".fade-up").forEach((el)=>{
-  gsap.from(el,{
-    ...fadeUp,
-    scrollTrigger:{
-      trigger:el,
-      scrub:true,
-      start:"top bottom",
-      end:"bottom bottom"
-    }
-  })
-})
-
-gsap.utils.toArray(".fade-right").forEach((el)=>{
-  gsap.from(el,{
-    ...fadeRight,
-    scrollTrigger:{
-      trigger:el,
-      scrub:true,
-      start:"top bottom",
-      end:"bottom bottom"
-    }
-  })
-})
-
-gsap.utils.toArray(".fade-left").forEach((el)=>{
-  gsap.from(el,{
-    ...fadeLeft,
-    x:-50,
-    scrollTrigger:{
-      trigger:el,
-      scrub:true,
-      start:"top bottom",
-      end:"bottom bottom"
-    }
-  })
-})
-
-gsap.to("#rotate",{
-  rotate:0,
-  scrollTrigger:{
-    trigger:"#story-section",
-    scrub:true,
-    start:"top bottom",
-    end:"bottom center",
+    y: 50,
+    ease: "power2.out",
+  };
+  //fade right
+  const fadeRight = {
+    autoAlpha: 0,
+    x: 50,
+    ease: "power2.out",
+  };
+  //fade left
+  const fadeLeft={
+    autoAlpha: 0,
+    ease:"power2.out"
   }
+  //fade left delayed
+  const headerElements = {
+    ...fadeLeft,
+    x:-10,
+    delay: 0.4,
+  };
+  
+  //fade left elements
+  gsap.from(".icon-link", headerElements);
+  gsap.from(".nav-item", headerElements);
+  
+  //fade up elements
+  gsap.from("h1 .word", {
+    ...fadeUp,
+    duration: 0.7,
+    stagger: 0.07,
+    ease: "back.out(1.7)",
+    delay: 0.4,
+  });
+  
+  gsap.from(".hero-image",{
+    scale:0.8,
+    ...fadeLeft,
+    delay:0.5
+  })
+  
+  gsap.from(".sub-title, .hero-button,.contact-card", {
+    ...fadeUp,
+    duration: 0.95,
+    delay: 0.6,
+    stagger: 0.1,
+  });
+  
+  
+  gsap.utils.toArray(".card:not(.service-card)").forEach((card) => {
+    gsap.from(card, {
+      scale: 0.7,
+      autoAlpha: 0,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: card,
+        scrub: true,
+        start: "top bottom",
+        end: "center bottom",       
+      }
+    });
+  });
+  
+  gsap.utils.toArray(".accordion-item").forEach((item)=>{
+    gsap.from(item,{
+      scale:0.5,
+      x:40,
+      opacity:0,
+      ease: "power4.out",
+      scrollTrigger: {
+        trigger: item,
+        scrub: true,
+        start: "top bottom",
+        end: "bottom 70%",       
+      }
+    })
+  })
+  
+  gsap.utils.toArray(".fade-up").forEach((el)=>{
+    gsap.from(el,{
+      ...fadeUp,
+      scrollTrigger:{
+        trigger:el,
+        scrub:true,
+        start:"top bottom",
+        end:"bottom bottom"
+      }
+    })
+  })
+  
+  gsap.utils.toArray(".fade-right").forEach((el)=>{
+    gsap.from(el,{
+      ...fadeRight,
+      scrollTrigger:{
+        trigger:el,
+        scrub:true,
+        start:"top bottom",
+        end:"bottom bottom"
+      }
+    })
+  })
+  
+  gsap.utils.toArray(".fade-left").forEach((el)=>{
+    gsap.from(el,{
+      ...fadeLeft,
+      x:-50,
+      scrollTrigger:{
+        trigger:el,
+        scrub:true,
+        start:"top bottom",
+        end:"bottom bottom"
+      }
+    })
+  })
+  
+  gsap.to("#rotate",{
+    rotate:0,
+    scrollTrigger:{
+      trigger:"#story-section",
+      scrub:true,
+      start:"top bottom",
+      end:"bottom center",
+    }
+  })
 })
